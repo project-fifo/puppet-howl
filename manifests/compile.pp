a1ac34a73c384d14ef85b3ca2110e50c3c23b342
Heinz N. Gies <heinz@licenser.net>
2016-12-21 02:17:32 +0100

    Fix build target

--- manifests/compile.pp
+++ manifests/compile.pp
@@ -1,6 +1,6 @@
 class howl::compile {
-  exec { 'make_release':
-    command => 'make release',
+  exec { 'make_rel':
+    command => 'make rel',
     cwd     => '/data/code/howl',
     path    => ['/opt/local/bin/', '/bin/'],
     # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
