756ed7d1d9c5c49027dc055f34f4423371ecb98c
Heinz N. Gies <heinz@licenser.net>
2016-12-21 02:16:33 +0100

    Add CWD

--- manifests/compile.pp
+++ manifests/compile.pp
@@ -1,7 +1,7 @@
 class howl::compile {
   exec { 'make_release':
     command => 'make release',
-    cwd     => '/data/howl',
+    cwd     => '/data/code/howl',
     path    => ['/opt/local/bin/', '/bin/'],
     # path    => [ '/usr/local/bin/', '/bin/' ],  # alternative syntax
   }
