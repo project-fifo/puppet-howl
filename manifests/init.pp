7f05b917e3848c374a8bddf1dbb3c4b4019e392b
Heinz N. Gies <heinz@licenser.net>
2016-12-21 22:11:17 +0100

    Execute config as part of howl

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,15 @@
 class howl {
   notify { 'Welcome to howl!': }
 
-  package { 'coreutils':
-    ensure => installed,
-  }
+  package { 'coreutils': ensure => installed }
 
-  package { 'sudo':
-    ensure => installed,
-  }
+  package { 'sudo': ensure => installed }
 
-  package { 'grep':
-    ensure => installed,
-  }
+  package { 'grep': ensure => installed }
 
-  class { howl::clone: }
+
+  class { howl::clone:   }
   class { howl::compile: }
-  class { howl::link: }
+  class { howl::link:    }
+  class { howl::config:  }
   }
