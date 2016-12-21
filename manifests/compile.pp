d85b16a8736be2470a0180322db367fcd336298c
Heinz N. Gies <heinz@licenser.net>
2016-12-21 03:38:15 +0100

    Add some requirements

--- manifests/compile.pp
+++ manifests/compile.pp
@@ -1,7 +1,7 @@
 class howl::compile {
 
   exec { 'make_rel':
-    require => [ File['/data/code/howl'] ],
+    require => [ Vcsrepo['/data/code/howl'] ],
     command => 'make rel',
     cwd     => '/data/code/howl',
     path    => ['/opt/local/bin/', '/bin/'],
